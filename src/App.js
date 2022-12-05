6e1f7bb7943eb5dcce6b361c08388256a381f341
BryanBorck <bry@Notebook-Bry.localdomain>
2022-12-05 02:02:41 -0300

    home developed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-
+import GlobalStyle from "./components/GlobalStyle";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home"
+import Connect from "./components/Connect";
 
 function App() {
   return (
-    <></>
+    <BrowserRouter>
+      <GlobalStyle/>
+      <Routes>
+        <Route path="/" exact element={<Home/>} />
+        <Route path="/connect" exact element={<Connect/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
